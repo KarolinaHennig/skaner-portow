0c312784401e9ad0bf03b74d9ba44918cdd08360
student <student@PC-217>
2017-05-25 16:21:51 +0200

    skanowanie portow

--- skaner-portow/Form1.Designer.cs
+++ skaner-portow/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.info = new System.Windows.Forms.ListBox();
+            this.skanuj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(12, 15);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(183, 20);
+            this.adres.TabIndex = 0;
+            this.adres.Text = "Adres";
+            // 
+            // info
+            // 
+            this.info.FormattingEnabled = true;
+            this.info.Location = new System.Drawing.Point(12, 41);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(818, 355);
+            this.info.TabIndex = 1;
+            // 
+            // skanuj
+            // 
+            this.skanuj.Location = new System.Drawing.Point(377, 401);
+            this.skanuj.Name = "skanuj";
+            this.skanuj.Size = new System.Drawing.Size(58, 27);
+            this.skanuj.TabIndex = 2;
+            this.skanuj.Text = "Skanuj";
+            this.skanuj.UseVisualStyleBackColor = true;
+            this.skanuj.Click += new System.EventHandler(this.skanuj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 440);
+            this.Controls.Add(this.skanuj);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.ListBox info;
+        private System.Windows.Forms.Button skanuj;
     }
 }
 
--- skaner-portow/Form1.cs
+++ skaner-portow/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace skaner_portow
 {
@@ -15,6 +16,27 @@ namespace skaner_portow
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void skanuj_Click(object sender, EventArgs e)
+        {
+            short[] ListaPortow = { 20, 21, 22, 24, 25, 27, 87, 55, 43, 87, 43 };
+            string host = adres.Text;
+            info.Items.Add("Skanuje: " + host);
+            info.Items.Add("To moze chwile potrwac");
+            foreach(short port in ListaPortow)
+            { this.Refresh();
+              try
+                {
+                    TcpClient klient = new TcpClient(host, port);
+                    info.Items.Add("port: " + port.ToString() + "jest otwarty"); 
+                }
+                catch
+                {
+                    info.Items.Add("port: " + port.ToString() + "jest zamkniety");
+                }
+            }
+
         }
     }
 }
